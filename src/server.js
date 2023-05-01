1a87f66232d997fa94255422e02c0acffe771fcc
Zidan Abraham <zidangit07@gmail.com>
2023-05-01 12:35:32 +0800

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import Hapi from '@hapi/hapi';
-// eslint-disable-next-line import/extensions
-import routes from './routes.js';
+import notes from './api/notes/index.js';
+import NotesServices from './services/inMemory/NotesService.js';
 
 const init = async () => {
+  const notesService = new NotesServices();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -13,7 +15,12 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,70 @@
+import { nanoid } from 'nanoid';
+
+export default class NotesServices {
+  constructor() {
+    this._notes = [];
+  }
+
+  addNote({ title, tags, body }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+      id,
+      title,
+      tags,
+      body,
+      createdAt,
+      updatedAt,
+    };
+
+    this._notes.push(newNote);
+
+    const isSuccess = this._notes.find((note) => note.id === id);
+    if (!isSuccess) {
+      throw new Error('Catatan gagal ditambahkan');
+    }
+
+    return id;
+  }
+
+  getNotes() {
+    return this._notes;
+  }
+
+  getNoteById(id) {
+    const note = this._notes.find((n) => n.id === id);
+    if (!note) {
+      throw new Error('Catatan tidak ditemukan');
+    }
+    return note;
+  }
+
+  editNoteById(id, { title, tags, body }) {
+    const index = this._notes.findIndex((note) => note.id === id);
+    const updatedAt = new Date().toISOString();
+
+    if (index === -1) {
+      throw new Error('Catatan gagal diperbarui, ID tidak ditemukan');
+    }
+
+    this._notes[index] = {
+      ...this._notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    return this._notes[index];
+  }
+
+  deleteNoteById(id) {
+    const index = this._notes.findIndex((note) => note.id === id);
+    if (index === -1) {
+      throw new Error('Catatan gagal dihapus, ID tidak ditemukan');
+    }
+    this._notes.slice(index, 1);
+  }
+}
