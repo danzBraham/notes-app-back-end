459d24fa846d2283b8edd2ab8d8c2f37ef5cd6bf
Zidan Abraham <zidangit07@gmail.com>
2023-05-11 18:16:36 +0800

    implement authentication

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const { ClientError } = require('./exceptions');
 
 const authentications = require('./api/authentications');
@@ -58,6 +59,28 @@ const init = async () => {
     }
 
     return h.continue;
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
