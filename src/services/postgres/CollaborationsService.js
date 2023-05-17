b38bcf83bb8c5d5a7379325880126086e1d657b6
Zidan Abraham <zidangit07@gmail.com>
2023-05-17 09:57:05 +0800

    implement server-side caching

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -3,8 +3,9 @@ const { nanoid } = require('nanoid');
 const { InvariantError } = require('../../exceptions');
 
 class CollaborationsService {
-  constructor() {
+  constructor(cacheService) {
     this._pool = new Pool();
+    this._cacheService = cacheService;
   }
 
   async addCollaboration(noteId, userId) {
@@ -20,6 +21,7 @@ class CollaborationsService {
       throw new InvariantError('Kolaborasi gagal ditambahkan');
     }
 
+    await this._cacheService.delete(`notes:${userId}`);
     return result.rows[0].id;
   }
 
@@ -33,6 +35,8 @@ class CollaborationsService {
     if (!rowCount) {
       throw new InvariantError('Kolaborasi gagal dihapus');
     }
+
+    await this._cacheService.delete(`notes:${userId}`);
   }
 
   async verifyCollaborator(noteId, userId) {
